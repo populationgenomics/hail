0ffd264992635e0fbe3219b6c168b29afcc6ca3b
vladsaveliev <vladislav.savelyev@populationgenomics.org.au>
2022-03-29 15:17:33 +1100

    Clean up merge artifact

--- hail/python/test/hail/helpers.py
+++ hail/python/test/hail/helpers.py
@@ -152,18 +152,6 @@ def skip_unless_service_backend(message='only relevant to service backend'):
     return wrapper
 
 
-def skip_unless_service_backend():
-    from hail.backend.service_backend import ServiceBackend
-    @decorator
-    def wrapper(func, *args, **kwargs):
-        if isinstance(hl.utils.java.Env.backend(), ServiceBackend):
-            return func(*args, **kwargs)
-        else:
-            raise unittest.SkipTest('requires ServiceBackend')
-
-    return wrapper
-
-
 fails_local_backend = pytest.mark.xfail(
     choose_backend() == 'local',
     reason="doesn't yet work on local backend",
