11b4a55862da39b90f895191cc44bd32dd92a046
Leonhard Gruenschloss <leonhard.gruenschloss@populationgenomics.org.au>
2022-05-12 10:12:24 +1000

    Add -XX:+HeapDumpOnOutOfMemoryError option for worker JVM

--- batch/batch/worker/worker.py
+++ batch/batch/worker/worker.py
@@ -2014,6 +2014,7 @@ class JVMContainer:
         command = [
             'java',
             f'-Xmx{heap_memory_mib}M',
+            '-XX:+HeapDumpOnOutOfMemoryError',
             '-cp',
             f'/jvm-entryway:/jvm-entryway/junixsocket-selftest-2.3.3-jar-with-dependencies.jar:{JVM.SPARK_HOME}/jars/*',
             'is.hail.JVMEntryway',
