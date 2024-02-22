0481a8aac701a2f162a5ee08d70735d5de5bf2ab
Michael Franklin <22381693+illusional@users.noreply.github.com>
2024-02-22 11:42:08 +1100

    Apply suggestions from code review
    Co-authored-by: John Marshall <john.marshall@populationgenomics.org.au>

--- batch/batch/front_end/front_end.py
+++ batch/batch/front_end/front_end.py
@@ -523,7 +523,7 @@ async def get_jobs_for_billing(request, userdata, batch_id):
 
 @routes.get('/api/v1alpha/batches/{batch_id}/jobs/{job_id}/resource_usage')
 @billing_project_users_only()
-async def get_job_resource_usage(request, _, batch_id):
+async def get_job_resource_usage(request: web.Request, _, batch_id: int) -> web.Response:
     """
     Get the resource_usage data for a job. The data is returned as a JSON object
     transformed from a pandas DataFrame using the 'split' orientation.
@@ -545,7 +545,7 @@ async def get_job_resource_usage(request, _, batch_id):
             ],
             "index":[0, 1, ...],
             "data": [[<records>]],
-        }
+        }, ...
     }
     """
 
